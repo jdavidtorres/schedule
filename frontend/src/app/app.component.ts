0c621df5b84bd2bc9e8beed75c1ed31bf488fe9f
Yisus <jesu_ti91@hotmail.com>
2025-01-06 15:16:24 -0500

    Refactor app structure by removing unused routing and module files, and reorganizing imports in app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,142 +1,185 @@
 import { ConfirmationService, MessageService } from 'primeng/api';
-
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
 
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MenubarModule } from 'primeng/menubar';
+import { MessageModule } from 'primeng/message';
+import { MessagesModule } from 'primeng/messages';
+import { TableModule } from 'primeng/table';
+import { TabViewModule } from 'primeng/tabview';
+import { TabsModule } from 'primeng/tabs';
+import { NgForm } from '@angular/forms';
 import { Event } from './models/Event';
 import { Instructor } from './models/Instructor';
 import { EventService } from './services/event.service';
 import { InstructorService } from './services/instructor.service';
+import { ToastModule } from 'primeng/toast';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DatePickerModule } from 'primeng/datepicker';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	imports: [
+		FormsModule,
+		BrowserModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		ToastModule,
+		InputTextModule,
+		ConfirmDialogModule,
+		DatePickerModule,
+		PanelModule,
+		TabsModule,
+		TabViewModule,
+		DropdownModule,
+		CalendarModule,
+		TableModule,
+		DialogModule,
+		ButtonModule,
+	],
+	styleUrls: ['./app.component.css'],
+	providers: [MessageService, ConfirmationService],
 })
 export class AppComponent implements OnInit {
+	title = 'Schedule';
 
-  title = 'Schedule';
+	instructors: Instructor[];
+	instructor: Instructor;
+	newInstructor: Instructor = {
+		id: null,
+		name: null,
+		surname: null,
+		birthday: null,
+		events: null,
+	};
 
-  instructors: Instructor[];
-  instructor: Instructor;
-  newInstructor: Instructor = {
-    id: null,
-    name: null,
-    surname: null,
-    birthday: null,
-    events: null
-  };
+	overall: number;
 
-  overall: number;
+	events: Event[];
+	event: Event = {
+		id: null,
+		title: null,
+		description: null,
+		start: null,
+		end: null,
+	};
+	eventItem: Event;
 
-  events: Event[];
-  event: Event = {
-    id: null,
-    title: null,
-    description: null,
-    start: null,
-    end: null
-  };
-  eventItem: Event;
+	options: any;
 
-  options: any;
+	eventDialogTable: boolean = false;
+	submitted: boolean = true;
 
-  eventDialogTable: boolean = false;
-  submitted: boolean = true;
+	maxDate: Date;
 
-  maxDate: Date;
+	constructor(
+		private instructorService: InstructorService,
+		private eventServices: EventService,
+		private messageService: MessageService,
+		private confirmationService: ConfirmationService
+	) {}
 
-  constructor(private instructorService: InstructorService, private eventServices: EventService, private messageService: MessageService, private confirmationService: ConfirmationService) { }
+	ngOnInit(): void {
+		this.eventServices.findAll().subscribe((events) => {
+			this.events = events;
+		});
 
-  ngOnInit(): void {
-    this.eventServices.findAll().subscribe(events => {
-      this.events = events;
-    });
+		this.instructorService.findAll().subscribe((instructors) => {
+			this.instructors = instructors;
+		});
+		this.maxDate = new Date();
+	}
 
-    this.instructorService.findAll().subscribe(instructors => {
-      this.instructors = instructors;
-    });
-    this.maxDate = new Date();
-  }
+	assignEvent(fomAssign: NgForm) {
+		this.instructorService.assignEventToInstructor(this.event, this.instructor.id).subscribe(
+			() => {
+				this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Event created' });
+				fomAssign.reset();
+			},
+			(error) => {
+				console.error('Something went wrong');
+				console.error(error);
+				this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
+			}
+		);
+	}
 
-  assignEvent(fomAssign: NgForm) {
-    this.instructorService.assignEventToInstructor(this.event, this.instructor.id).subscribe(
-      (result: any) => {
-        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Event created' });
-        fomAssign.reset();
-      },
-      error => {
-        console.error("Something went wrong");
-        console.error(error);
-        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
-      });
-  }
+	getOverall(idInstructor: string) {
+		this.instructorService.getOverall(idInstructor).subscribe((result) => {
+			this.overall = result;
+		});
+	}
 
-  getOverall(idInstructor: string) {
-    this.instructorService.getOverall(idInstructor).subscribe(result => {
-      this.overall = result;
-    });
-  }
+	editEvent(eventEdit: Event) {
+		this.eventItem = {
+			id: eventEdit.id,
+			title: eventEdit.title,
+			description: eventEdit.description,
+			start: eventEdit.start,
+			end: eventEdit.end,
+		};
+		this.eventDialogTable = true;
+	}
 
-  editEvent(eventEdit: Event) {
-    this.eventItem = {
-      id: eventEdit.id,
-      title: eventEdit.title,
-      description: eventEdit.description,
-      start: eventEdit.start,
-      end: eventEdit.end
-    };
-    this.eventDialogTable = true;
-  }
+	saveUpdated(eventDialog: Event) {
+		this.submitted = true;
+		console.log(this.eventItem.title);
+		console.log(this.eventItem.description);
+		console.log(eventDialog.start);
+		console.log(eventDialog.end);
+		this.eventServices.updateEvent(this.instructor.id, this.eventItem.id, this.eventItem).subscribe();
+		this.instructorService.getInstructor(this.instructor.id).subscribe((instructor) => {
+			this.instructor = instructor;
+			this.events = this.instructor.events;
+		});
+		this.events = [...this.events];
+		this.eventDialogTable = false;
+	}
 
-  saveUpdated(eventDialog: Event) {
-    this.submitted = true;
-    console.log(this.eventItem.title);
-    console.log(this.eventItem.description);
-    console.log(eventDialog.start);
-    console.log(eventDialog.end);
-    this.eventServices.updateEvent(this.instructor.id, this.eventItem.id, this.eventItem).subscribe();
-    this.instructorService.getInstructor(this.instructor.id).subscribe(instructor => {
-      this.instructor = instructor;
-      this.events = this.instructor.events;
-    });
-    this.events = [...this.events];
-    this.eventDialogTable = false;
-  }
+	deleteEvent(event: Event) {
+		this.confirmationService.confirm({
+			message: 'Are you sure you want to delete it?',
+			header: 'Confirmation',
+			icon: 'pi pi-exclamation-triangle',
+			accept: () => {
+				this.eventServices.deleteEvent(event.id, this.instructor.id).subscribe((result: any) => {
+					this.instructorService.getInstructor(this.instructor.id).subscribe((instructor) => {
+						this.instructor = instructor;
+						this.events = this.instructor.events;
+					});
+				});
+				this.events = this.instructor.events;
+			},
+		});
+	}
 
-  deleteEvent(event: Event) {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete it?',
-      header: 'Confirmation',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.eventServices.deleteEvent(event.id, this.instructor.id).subscribe(
-          (result: any) => {
-            this.instructorService.getInstructor(this.instructor.id).subscribe(instructor => {
-              this.instructor = instructor;
-              this.events = this.instructor.events;
-            });
-          }
-        );
-        this.events = this.instructor.events;
-      }
-    });
-  }
+	hideDialog() {
+		this.eventDialogTable = false;
+		this.submitted = false;
+	}
 
-  hideDialog() {
-    this.eventDialogTable = false;
-    this.submitted = false;
-  }
-
-  saveInstructor(instructor: Instructor) {
-    this.instructors.push(instructor);
-    this.instructorService.saveInstructor(instructor).subscribe();
-    this.newInstructor = {
-      id: null,
-      name: null,
-      surname: null,
-      birthday: null,
-      events: null
-    };
-  }
+	saveInstructor(instructor: Instructor) {
+		this.instructors.push(instructor);
+		this.instructorService.saveInstructor(instructor).subscribe();
+		this.newInstructor = {
+			id: null,
+			name: null,
+			surname: null,
+			birthday: null,
+			events: null,
+		};
+	}
 }
